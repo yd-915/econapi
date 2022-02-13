4d741e57d52476671a32b0484097a1a752fdc228
Artem Abdullin <artemleelooai@gmail.com>
2022-02-13 20:59:33 +0200

    Added payment method Stripe

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
